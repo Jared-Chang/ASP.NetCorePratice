7b4cbf84bdc20192b122324533cda7f1de2b1910
Jared.Chang <Jared.Chang@titansoft.com.sg>
2020-06-03 23:43:36 +0800

    feat: add get current ip from ipify

--- SKA/Controllers/WhereAmIController.cs
+++ SKA/Controllers/WhereAmIController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace SKA.Controllers
 {
@@ -11,19 +13,51 @@ namespace SKA.Controllers
     [ApiController]
     public class WhereAmIController : ControllerBase
     {
+        private readonly IGeoIpService _geoIpService;
+
+        public WhereAmIController(IGeoIpService geoIpService)
+        {
+            _geoIpService = geoIpService;
+        }
+
         public async Task<IActionResult> Index()
         {
+            var currentIp = await _geoIpService.GetCurrentIpAsync();
             return Ok(new
             {
-                Ip = "8.8.8.8",
+                Ip = currentIp,
                 CountryCode = "TW"
             });
         }
     }
 
+    public interface IGeoIpService
+    {
+        Task<string> GetCurrentIpAsync();
+    }
+
+    public class GeoIpService : IGeoIpService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public GeoIpService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> GetCurrentIpAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var result = await httpClient.GetAsync("https://api.ipify.org?format=json");
+            result.EnsureSuccessStatusCode();
+            var currentIp = JsonConvert.DeserializeObject<GeoResponse>(await result.Content.ReadAsStringAsync());
+            return currentIp.Ip;
+
+        }
+    }
+
     public class GeoResponse
     {
-        public string CountryCode { get; set; }
         public string Ip { get; set; }
     }
 }
--- SKA/Startup.cs
+++ SKA/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SKA.Controllers;
 
 namespace SKA
 {
@@ -24,6 +25,8 @@ namespace SKA
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient();
+            services.AddTransient<IGeoIpService, GeoIpService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
