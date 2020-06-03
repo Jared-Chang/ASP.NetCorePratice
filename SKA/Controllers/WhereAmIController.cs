5998fcc76dcaa610a68605c3a95ac9092b7e36cd
Jared.Chang <Jared.Chang@titansoft.com.sg>
2020-06-03 23:20:54 +0800

    init: api controller with hardcode response

---
+++ SKA/Controllers/WhereAmIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SKA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhereAmIController : ControllerBase
+    {
+        public async Task<ActionResult<GeoResponse>> Index()
+        {
+            return new GeoResponse()
+            {
+                Ip = "8.8.8.8",
+                CountryCode = "TW"
+            };
+        }
+    }
+
+    public class GeoResponse
+    {
+        public string CountryCode { get; set; }
+        public string Ip { get; set; }
+    }
+}
